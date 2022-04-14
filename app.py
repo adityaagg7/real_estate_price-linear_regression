23d3b2157f2c659bd5d77ef199395c4a665508f3
tida1732 <79642133+tida1732@users.noreply.github.com>
2022-04-14 13:10:07 +0530

    feat:Update app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as pt
+import hvplot
+
+df = pd.read_csv('data/Real estate.csv')
+df = df.drop_duplicates(keep="first")
+print(df.shape)
+if np.isnan(df.values).any():
+    df.fillna(df.means())
+
+print(df.head())
+x = df.iloc[:, 2:8].values
+y = df.iloc[:, -1].values
+
+x_tr, x_t, y_tr, y_t = train_test_split(x, y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()
+regressor.fit(x_tr, y_tr)
+y_p = regressor.predict(x_t)
+
+res_error = y_t-y_p
+pt.scatter(y_t, res_error, color="red")
+pt.show()
